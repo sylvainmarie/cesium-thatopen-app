c49eac55a39a71ee43c851b4c7e24b2dbe4e86aa
Sylvain MARIE <sylvain.marie@eurostep.com>
2024-12-13 10:28:07 +0100

    Dynamically bundling the Cesium dependencies using Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/cesium/Build/Cesium/Workers',
+          dest: 'Cesium'
+        },
+        {
+          src: 'node_modules/cesium/Build/Cesium/ThirdParty',
+          dest: 'Cesium'
+        },
+        {
+          src: 'node_modules/cesium/Build/Cesium/Assets',
+          dest: 'Cesium'
+        },
+        {
+          src: 'node_modules/cesium/Build/Cesium/Widgets',
+          dest: 'Cesium'
+        }
+      ]
+    })
+  ]
 })
